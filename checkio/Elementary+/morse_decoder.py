c5230b0b9dddd70af67fe6843281eca815cf4a2f
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2022-10-04 18:50:58 +0300

    add morse_decoder

---
+++ checkio/Elementary+/morse_decoder.py
@@ -0,0 +1,71 @@
+from email import message
+
+
+MORSE = {
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+    "-----": "0",
+    ".----": "1",
+    "..---": "2",
+    "...--": "3",
+    "....-": "4",
+    ".....": "5",
+    "-....": "6",
+    "--...": "7",
+    "---..": "8",
+    "----.": "9",
+}
+
+
+def morse_decoder(code: str) -> str:
+    # replace this for solution
+    import re
+
+    list_morse = re.findall(r"[-.]+|   ", code)
+    message = ""
+
+    for item in list_morse:
+        
+        if item == "   ":
+            message += " "
+        else:
+            message += MORSE[item]
+ 
+    return message.capitalize()
+
+
+print("Example:")
+print(morse_decoder("... --- -- .   - . -..- -"))
+
+assert morse_decoder("... --- -- .   - . -..- -") == "Some text"
+assert (
+    morse_decoder(
+        "..   .-- .- ...   -... --- .-. -.   .. -.   .---- ----. ----. -----")
+    == "I was born in 1990"
+)
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
