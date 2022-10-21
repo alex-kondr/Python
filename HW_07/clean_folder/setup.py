ecfd0cc2938d16b0305acbf90c741e3e12672f7c
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2022-10-21 14:10:54 +0300

    sord folder

---
+++ HW_07/clean_folder/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="clean",
+    version="1",
+    description="Sorts files for extention",
+    url="https://github.com/alex-kondr/Python/tree/master/HW_06/HW_06_sort/clean_folder",
+    autor="Alex Kondr",
+    packages=find_namespace_packages(),
+    entry_points={"console_scripts": ["clean-folder = clean_folder.clean:main"]},
+)
