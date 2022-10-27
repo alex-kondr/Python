c81c97a3e88e4da51126a07211eee8c8cdbd87e2
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2022-10-27 14:05:21 +0300

    edit hw_08

--- HW_08/hw_08.py
+++ HW_08/hw_08.py
@@ -1,17 +1,6 @@
 from datetime import datetime, timedelta
 
 
-users = [
-    {"name": "Bill+", "birthday": datetime(year=2002, month=11, day=3)}, #Thursday
-    {"name": "Jill+", "birthday": datetime(year=1996, month=10, day=30, hour=10, minute=32)}, #Monday
-    {"name": "Kim+", "birthday": datetime(year=2005, month=11, day=1)}, #Tuesday
-    {"name": "Jan+", "birthday": datetime(year=2007, month=10, day=30, hour=6)}, #Monday
-    {"name": "Foo", "birthday": datetime(year=1997, month=8, day=2)}, #-
-    {"name": "Bar", "birthday": datetime(year=2009, month=1, day=14)}, #-
-    {"name": "Baz+", "birthday": datetime(year=1989, month=10, day=29, hour=18, minute=31, second=46)} #Monday
-]
-
-
 def get_birthdays_per_week(users: list) -> dict:
 
     birth_users = {}
@@ -52,9 +41,4 @@ def day_of_celebration(byrthday: datetime) -> str:
         day = "Monday"
 
     return day
-
-
-print(get_birthdays_per_week(users))
-
-
 
