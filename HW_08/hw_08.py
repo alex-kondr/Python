e587da9a1d1bf390dd98e15e2caf8767a30b418c
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2022-10-28 10:30:32 +0300

    edit hw_08

--- HW_08/hw_08.py
+++ HW_08/hw_08.py
@@ -5,17 +5,17 @@ def get_birthdays_per_week(users: list) -> dict:
 
     birth_users = {}
 
-    for u in users:
+    for user in users:
 
-        day = day_of_celebration(u["birthday"])
+        day = day_of_celebration(user["birthday"])
 
         if day and birth_users.get(day):
-            birth_users[day].append(u["name"])
+            birth_users[day].append(user["name"])
 
         elif day:
-            birth_users[day] = [u["name"]]
+            birth_users[day] = [user["name"]]
 
-    return birth_users
+    print(birth_users)
 
 def day_of_celebration(birthday: datetime) -> str:
     # If birthday is the next week returns day of week
@@ -26,12 +26,13 @@ def day_of_celebration(birthday: datetime) -> str:
     next_week = now + timedelta(weeks=1)
     birthday = birthday.replace(year=now.year)    
 
-    if birthday.isocalendar().week == next_week.isocalendar().week:
+    if birthday.isocalendar().week == next_week.isocalendar().week and \
+        birthday.weekday() not in (5, 6):
 
         day = birthday.strftime("%A")
 
     elif birthday.isocalendar().week == now.isocalendar().week and \
-        (birthday.weekday() == 5 or birthday.weekday() == 6):
+        birthday.weekday() in (5, 6):
        
         day = "Monday"
 
