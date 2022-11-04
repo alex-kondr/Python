c5e7871ad32f61c29b341f9db7c137171a7d4b90
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2022-11-04 14:43:52 +0200

    bild pack

---
+++ HW_09/console_bot/build/lib/build/lib/console_bot/input_error.py
@@ -0,0 +1,22 @@
+def input_error(func):
+    def inner(data: str) -> str:
+
+        message = "\n" + "-" * 55 + "\n"
+
+        try:
+            message += func(data)
+
+        except TypeError:
+            message += func()
+
+        except ValueError:
+            message += "\nEnter valid command.\nPlease enter help for more information.\n"
+        
+        except KeyError:
+            message += "\nThis name not exists.\n"
+
+        message += "\n" + "-" * 55 + "\n"
+
+        return message
+
+    return inner
