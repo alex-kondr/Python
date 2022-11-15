efad4121b4857185b23fbec10486b38edcd27bb3
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2022-11-15 13:32:39 +0200

    add criteria for HW_11

--- HW_11/console_bot/console_bot/address_book.py
+++ HW_11/console_bot/console_bot/address_book.py
@@ -1,5 +1,5 @@
 from collections import UserDict
-from datetime import datetime, timedelta
+from datetime import datetime
 import re
 
 
@@ -16,11 +16,14 @@ class Birthday(Field):
 
     @value.setter
     def value(self, birthday: str):
-        try:
-            self._value = datetime.strptime(birthday, "%d.%m")
-        except ValueError:
-            raise ValueError("Birthday not valid.\n"
+
+        birthday = re.search("\d{2}\.\d{2}", birthday)  #type: ignore
+
+        if not birthday:
+            raise ValueError("Birthday not valid.\n"\
                 "The Birthday should look like '01.01'")
+        
+        self._value = datetime.strptime(birthday.group(), "%d.%m")  #type: ignore
 
 
 class Name(Field):
@@ -34,7 +37,7 @@ class Name(Field):
         return self._value
 
     @value.setter
-    def value(self, name):
+    def value(self, name: str):
         if type(name) == str:
             self._value = name
 
@@ -46,14 +49,14 @@ class Phone(Field):
         return self._value
 
     @value.setter
-    def value(self, phone):
-        phone = re.search(r"\+380\d{9}", phone)
+    def value(self, phone: str):
+        phone = re.search(r"\+380\d{9}", phone)  # type: ignore
 
         if not phone:
             raise ValueError("Phone number not valid.\n"\
                 "The phone number should look like +380123456789")
         
-        self._value = phone.group()
+        self._value = phone.group()  # type: ignore
 
 
 class Record():
@@ -63,9 +66,6 @@ class Record():
         self.name = name
         self.phones = []
 
-    def __next__(self):
-        pass
-
     def add_phone(self, phone: Phone):
         self.phones.append(phone)
 
@@ -73,7 +73,14 @@ class Record():
         self.phones[number_in_list].mobile_phone = new_phone
 
     def days_to_birthday(self):
-        pass
+
+        if not self.birthday.value:
+            return "Birthday not specified"
+
+        now_date = datetime.now()
+        birthday = self.birthday.value.replace(year=now_date.year)
+
+        return  (birthday - now_date).days        
 
     def remove_phone(self, number_in_list: int):
         return self.phones.pop(number_in_list)
@@ -81,14 +88,25 @@ class Record():
 
 class AddressBook(UserDict):
 
-    def __iter__(self):
-        pass
-
     def add_record(self, record: Record):
         self.data.update({record.name.value: record})
+
+    def iterator(self, N: int):
+        i = 0
+        data = {}
+        
+        for n, (name, record) in enumerate(self.data.items()):
+            data.update({name: record})
+            i += 1
+
+            if i == N or n == len(self.data) - 1:
+                yield data
+                data.clear()
+                i = 0
 
     def get_contact(self, name: str):
         return self.data.get(name)
 
     def list_contacts(self) -> dict:
         return self.data
+
