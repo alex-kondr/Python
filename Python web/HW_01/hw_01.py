9526d74302a9a32cdc2969fe37b3fda65640d262
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2022-12-01 15:00:30 +0200

    first part

--- Python web/HW_01/hw_01.py
+++ Python web/HW_01/hw_01.py
@@ -1,3 +1,11 @@
+#1
+
+class SerializationInterface(type):
+    pass
+
+#2
+
+
 class Meta(type):
     children_number = 0
     class_number = 0
@@ -8,7 +16,8 @@ class Meta(type):
         return type.__new__(*args)
 
     def __init__(self, *args):
-        self.class_number += 1
+        self.class_number = Meta.class_number
+        Meta.class_number += 1
 
 
 Meta.children_number = 0
@@ -25,6 +34,6 @@ class Cls2(metaclass=Meta):
         self.data = data
 
 
-# assert (Cls1.class_number, Cls2.class_number) == (0, 1)
-# a, b = Cls1(""), Cls2("")
-# assert (a.class_number, b. class_numer) == (0, 1)
+assert (Cls1.class_number, Cls2.class_number) == (0, 1)
+a, b = Cls1(""), Cls2("")
+assert (a.class_number, b.class_number) == (0, 1)
