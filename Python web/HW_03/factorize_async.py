49b261d7d146ee71ce6ffb125737959cd38fcfff
alex_kondr <alex_kondr@outlook.com>
2023-01-26 20:26:24 +0200

    save

--- Python web/HW_03/factorize_async.py
+++ Python web/HW_03/factorize_async.py
@@ -1,45 +1,62 @@
 from time import time
 import multiprocessing
-import concurrent.futures
+from concurrent.futures import ProcessPoolExecutor
 
 
 time_start = time()
-cpu_count = multiprocessing.cpu_count()
-# print(cpu_count)
+CPU_COUNT = multiprocessing.cpu_count()
+SEPARATOR = 1000000
+temp = 0
 
 
-def factorize(*numbers: int) -> list[int]:    
-    list_of_divisors = []
+def factorize(number):
+    preliminary_list = []
     
-    for number in numbers:
-        list_of_divisors_for_number = []
-        
+    if number > SEPARATOR:
+        for i in range(number-SEPARATOR, number+1):
+            if not temp % i:
+                preliminary_list.append(i)        
+    
+    else:
         for i in range(1, number+1):
             if not number % i:
-                list_of_divisors_for_number.append(i)
-        
-        list_of_divisors.append(list_of_divisors_for_number)
+                preliminary_list.append(i)
+                
+    return preliminary_list
+
+
+def factorize_one_number(number):
+    global temp
+    temp = number
+    
+    numbers = [i for i in range(1, number, SEPARATOR)
+               if i > SEPARATOR] + [number]
+    list_of_divisors = []
+
+    with ProcessPoolExecutor(CPU_COUNT) as executor:
+        for preliminary_list in executor.map(factorize, numbers):
+            list_of_divisors += preliminary_list
             
     return list_of_divisors
 
 
-# numbers = (128, 255, 99999, 106510600)
-numb = 106510600
-numbers = [i for i in range(1, numb, 1000)]
+def factorize_numbers(*numbers: int) -> list[int]:
+    list_of_divisors = []
 
-# print(numbers)
+    for number in numbers:
+        list_of_divisors.append(factorize_one_number(number))
 
-
-# if __name__ == '__main__':
-#     with concurrent.futures.ProcessPoolExecutor(cpu_count) as executor:
-#         for number, list_of_divisors in zip(numbers, executor.map(factorize, numbers)):
-#             print(f'{number} is list_of_divisors:{list_of_divisors}')
+    return list_of_divisors
 
 
-
-# assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-# assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-# assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-# assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
-
-print(time()-time_start)
+if __name__ == "__main__":
+    
+    a, b, c, d = factorize_numbers(128, 255, 99999, 10651060)
+    
+    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316,
+                380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+    
+    print(time()-time_start)
