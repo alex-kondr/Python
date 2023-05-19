b7ad1d95e2266dbf8644eba76d6ee539f791e641
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2023-05-19 10:02:19 +0300

    done

---
+++ freelance/simple_iteration/main.py
@@ -0,0 +1,37 @@
+x = (21, 1, 2, 1.5)
+
+
+def func(x1, x2, x3, x4):
+    x1_next = 42/5 - (1/5) * x2 + (1/5) * x3 - (1/5) * x4
+    x2_next = -2 + (1/4) * x1 + (1/4) * x3 - (1/4) * x4
+    x3_next = 1/4 + (1/4) * x1 - (1/4) * x2 - (1/4) * x4
+    x4_next = -(16/5) + (1/5) * x1 + (2/5) * x2 + (1/5) * x3
+    return x1_next, x2_next, x3_next, x4_next
+
+
+def deviation(x: tuple, x_next: tuple):
+    difference = []
+    for xi, x_nexti in zip(x, x_next):
+        difference.append(abs(xi-x_nexti))
+    return max(difference)
+
+
+def iteration(x, count_iteration):
+    local_x = x
+    for _ in range(count_iteration):
+        x_start = local_x
+        local_x = func(*x_start)
+        error = deviation(x_start, local_x)
+
+    print(f"{local_x=}, {error=}")
+
+
+iteration(x, 2)
+
+
+
+
+
+
+
+
